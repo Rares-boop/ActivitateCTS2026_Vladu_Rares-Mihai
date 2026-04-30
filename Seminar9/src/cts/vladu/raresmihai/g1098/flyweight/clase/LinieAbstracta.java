00f7984e74e183d5ffe22d10c4210167a0cda963
Rares <vladurares@gmail.com>
2026-04-30 19:09:03 +0300

    aici seminar care a durat mai mult

---
+++ Seminar9/src/cts/vladu/raresmihai/g1098/flyweight/clase/LinieAbstracta.java
@@ -0,0 +1,6 @@
+package cts.vladu.raresmihai.g1098.flyweight.clase;
+
+public interface LinieAbstracta {
+    void descriereLinie(Autobuz autobuz);
+    void afiseazaNumarMaximPasageriPeLinie(Autobuz autobuz);
+}
---
+++ Seminar9/src/cts/vladu/raresmihai/g1098/proxy/clase/TipCursa.java
@@ -0,0 +1,6 @@
+package cts.vladu.raresmihai.g1098.proxy.clase;
+
+public enum TipCursa {
+    CURSA_NORMALA,
+    CURSA_SPECIALA
+}
